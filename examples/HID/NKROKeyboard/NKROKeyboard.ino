81b69bd9f5f34888838aae2f7882f31e75c45e51
hlcm <103294894+hlcmOxO@users.noreply.github.com>
2022-10-16 19:31:06 +0800

    led not working

--- examples/HID/NKROKeyboard/NKROKeyboard.ino
+++ examples/HID/NKROKeyboard/NKROKeyboard.ino
@@ -37,6 +37,28 @@ Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROT
 
 hid_nkrokeyboard_report_t kb;
 
+// Output report callback for LED indicator such as Caplocks
+void hid_report_callback(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
+{
+  (void) report_id;
+  (void) bufsize;
+
+  // LED indicator is output report with only 1 byte length
+  if ( report_type != HID_REPORT_TYPE_OUTPUT ) return;
+
+  // The LED bit map is as follows: (also defined by KEYBOARD_LED_* )
+  // Kana (4) | Compose (3) | ScrollLock (2) | CapsLock (1) | Numlock (0)
+  uint8_t ledIndicator = buffer[0];
+
+  // turn on LED if capslock is set
+  digitalWrite(LED_BUILTIN, ledIndicator & KEYBOARD_LED_CAPSLOCK);
+
+#ifdef PIN_NEOPIXEL
+  pixels.fill(ledIndicator & KEYBOARD_LED_CAPSLOCK ? 0xff0000 : 0x000000);
+  pixels.show();
+#endif
+}
+
 void setup()
 {
 #if defined(ARDUINO_ARCH_MBED) && defined(ARDUINO_ARCH_RP2040)
@@ -51,6 +73,8 @@ void setup()
   // usb_hid.setPollInterval(2);
   // usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
   // usb_hid.setStringDescriptor("TinyUSB Keyboard");
+
+  usb_hid.setReportCallback(NULL, hid_report_callback);
 
   usb_hid.begin();
 
